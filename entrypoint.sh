ea279d6221dd0d7855efd4bb003a6320c72f302b
BlackEagle <ike.devolder@gmail.com>
2018-01-23 16:46:36 +0100

    why did i have to mess around with /bin/sh for cron?
    Signed-off-by: BlackEagle <ike.devolder@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 if [ "${CRON}" ]; then
-    exec /command.sh go-cron -s "${CRON}" -- /bin/sh -c "$@"
+    exec /command.sh go-cron -s "${CRON}" -- "$@"
 else
     exec /command.sh "$@"
 fi
